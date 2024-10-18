afa20fd54648efa56bfec9217cdd130220ee0acf
Karim <benkiranemedkarim@gmail.com>
2024-10-18 21:44:06 +0100

    Added error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { FilmDetailsComponent } from './film-details/film-details.component';
 import { ListeFilmsComponent } from './liste-films/liste-films.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'connexion',
     component: ConnexionComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
