35427e76913657c27d8950f03c900832b31e16d8
Karim <benkiranemedkarim@gmail.com>
2024-12-09 21:14:51 +0100

    Added subscription unsubscribe for the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,13 @@
-import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { FilmsHelperService } from '../films-helper.service';
 
@@ -10,21 +18,25 @@ import { FilmsHelperService } from '../films-helper.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   filmsHelper = inject(FilmsHelperService);
   authService: AuthService = inject(AuthService);
   router = inject(Router);
 
   favCount: number = 0;
   @Output() onClearSearch = new EventEmitter();
+  countSubscription!: Subscription;
 
   ngOnInit(): void {
     this.updateCount();
-    this.filmsHelper.update$.subscribe((result) => {
+    this.countSubscription = this.filmsHelper.update$.subscribe((result) => {
       if (result) {
         this.updateCount();
       }
     });
+  }
+  ngOnDestroy(): void {
+    this.countSubscription.unsubscribe();
   }
   updateCount() {
     if (this.authService.userId) {
